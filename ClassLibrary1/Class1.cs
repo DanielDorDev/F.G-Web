3a12c0a51f5b0a1fad34c08d63a19117d3b31fe5
dani21123 <danidor59@gmail.com>
2019-06-19 20:44:30 +0300

    Revert "Update source"
    This reverts commit 95f5755db25e5cc252105f50e81ecd28f295e887.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int value = 3;
+    if (fork() != 0)
+    {
+
+        wait(&value);
+    }
+    else
+    {
+        exit(value);
+
+    }
+    value = WEXITSTATUS(value);
+    value++;
+    printf("%d", value);
+    return value;
+}
